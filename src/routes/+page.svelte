e0ac9e12377cada22023267371c18a2e08a4f65c
elduke717 <spacematcha@proton.me>
2023-07-13 15:36:16 -0400

    add outline to main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,10 +9,30 @@
     <a href="/algorithms">Algorithms</a>
   </nav>
 
-<h1>Data Structures and Algorithms</h1>
-<h2>This site is a guide of common data-structures and algorithms</h2>
-<ul>
-    <li>Binary Search Trees</li>
-    <li>Linked Lists</li>
-</ul>
+  <h1>Data Structures and Algorithms</h1>
+  <h2>This site is a guide to common data-structures and algorithms</h2>
+  
+  <h3>Table of Contents</h3>
+  <ul>
+    <li><a href="/tree">1. Binary Search Trees</a></li>
+    <li><a href="/ll">2. Linked Lists</a></li>
+    <li><a href="/stacks">3. Stacks</a></li>
+    <li><a href="/queues">4. Queues</a></li>
+    <li><a href="/hash-tables">5. Hash Tables</a></li>
+    <li><a href="/graphs">6. Graphs</a></li>
+    <li><a href="/algorithms">7. Algorithms</a></li>
+  </ul>
 
+
+<!-- # Data Structures and Algorithms
+
+This site is a guide to common data-structures and algorithms.
+
+## Table of Contents
+1. [Binary Search Trees](/tree)
+2. [Linked Lists](/ll)
+3. [Stacks](/stacks)
+4. [Queues](/queues)
+5. [Hash Tables](/hash-tables)
+6. [Graphs](/graphs)
+7. [Algorithms](/algorithms) -->
